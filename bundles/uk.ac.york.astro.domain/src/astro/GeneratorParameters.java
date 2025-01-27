046e2f684220d814d13dbd718c534c95fefb8866
idp505 <idp505@york.ac.uk>
2025-01-27 18:23:58 +0000

    Revert "undo add domain projects"
    This reverts commit 8ed6f948f86716f8805934347bafc3e204a0a171.

---
+++ bundles/uk.ac.york.astro.domain/src/astro/GeneratorParameters.java
@@ -0,0 +1,184 @@
+/**
+ */
+package astro;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Generator Parameters</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * </p>
+ * <ul>
+ *   <li>{@link astro.GeneratorParameters#getEmfProject <em>Emf Project</em>}</li>
+ *   <li>{@link astro.GeneratorParameters#getEmfEditProject <em>Emf Edit Project</em>}</li>
+ *   <li>{@link astro.GeneratorParameters#getPomVersion <em>Pom Version</em>}</li>
+ *   <li>{@link astro.GeneratorParameters#getSiriusWebVersion <em>Sirius Web Version</em>}</li>
+ *   <li>{@link astro.GeneratorParameters#getProjectName <em>Project Name</em>}</li>
+ *   <li>{@link astro.GeneratorParameters#getProjectPath <em>Project Path</em>}</li>
+ *   <li>{@link astro.GeneratorParameters#getTargetPackage <em>Target Package</em>}</li>
+ * </ul>
+ *
+ * @see astro.AstroPackage#getGeneratorParameters()
+ * @model
+ * @generated
+ */
+public interface GeneratorParameters extends EObject {
+	/**
+	 * Returns the value of the '<em><b>Emf Project</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Emf Project</em>' attribute.
+	 * @see #setEmfProject(String)
+	 * @see astro.AstroPackage#getGeneratorParameters_EmfProject()
+	 * @model
+	 * @generated
+	 */
+	String getEmfProject();
+
+	/**
+	 * Sets the value of the '{@link astro.GeneratorParameters#getEmfProject <em>Emf Project</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Emf Project</em>' attribute.
+	 * @see #getEmfProject()
+	 * @generated
+	 */
+	void setEmfProject(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Emf Edit Project</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Emf Edit Project</em>' attribute.
+	 * @see #setEmfEditProject(String)
+	 * @see astro.AstroPackage#getGeneratorParameters_EmfEditProject()
+	 * @model
+	 * @generated
+	 */
+	String getEmfEditProject();
+
+	/**
+	 * Sets the value of the '{@link astro.GeneratorParameters#getEmfEditProject <em>Emf Edit Project</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Emf Edit Project</em>' attribute.
+	 * @see #getEmfEditProject()
+	 * @generated
+	 */
+	void setEmfEditProject(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Pom Version</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Pom Version</em>' attribute.
+	 * @see #setPomVersion(String)
+	 * @see astro.AstroPackage#getGeneratorParameters_PomVersion()
+	 * @model
+	 * @generated
+	 */
+	String getPomVersion();
+
+	/**
+	 * Sets the value of the '{@link astro.GeneratorParameters#getPomVersion <em>Pom Version</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Pom Version</em>' attribute.
+	 * @see #getPomVersion()
+	 * @generated
+	 */
+	void setPomVersion(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Sirius Web Version</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Sirius Web Version</em>' attribute.
+	 * @see #setSiriusWebVersion(String)
+	 * @see astro.AstroPackage#getGeneratorParameters_SiriusWebVersion()
+	 * @model
+	 * @generated
+	 */
+	String getSiriusWebVersion();
+
+	/**
+	 * Sets the value of the '{@link astro.GeneratorParameters#getSiriusWebVersion <em>Sirius Web Version</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Sirius Web Version</em>' attribute.
+	 * @see #getSiriusWebVersion()
+	 * @generated
+	 */
+	void setSiriusWebVersion(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Project Name</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Project Name</em>' attribute.
+	 * @see #setProjectName(String)
+	 * @see astro.AstroPackage#getGeneratorParameters_ProjectName()
+	 * @model
+	 * @generated
+	 */
+	String getProjectName();
+
+	/**
+	 * Sets the value of the '{@link astro.GeneratorParameters#getProjectName <em>Project Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Project Name</em>' attribute.
+	 * @see #getProjectName()
+	 * @generated
+	 */
+	void setProjectName(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Project Path</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Project Path</em>' attribute.
+	 * @see #setProjectPath(String)
+	 * @see astro.AstroPackage#getGeneratorParameters_ProjectPath()
+	 * @model
+	 * @generated
+	 */
+	String getProjectPath();
+
+	/**
+	 * Sets the value of the '{@link astro.GeneratorParameters#getProjectPath <em>Project Path</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Project Path</em>' attribute.
+	 * @see #getProjectPath()
+	 * @generated
+	 */
+	void setProjectPath(String value);
+
+	/**
+	 * Returns the value of the '<em><b>Target Package</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Target Package</em>' attribute.
+	 * @see #setTargetPackage(String)
+	 * @see astro.AstroPackage#getGeneratorParameters_TargetPackage()
+	 * @model
+	 * @generated
+	 */
+	String getTargetPackage();
+
+	/**
+	 * Sets the value of the '{@link astro.GeneratorParameters#getTargetPackage <em>Target Package</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Target Package</em>' attribute.
+	 * @see #getTargetPackage()
+	 * @generated
+	 */
+	void setTargetPackage(String value);
+
+} // GeneratorParameters
